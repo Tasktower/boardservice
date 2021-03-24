6d98cc057855c377276b1eaababe49f2f3a794d8
Oren Ben-Meir <orenbm11@gmail.com>
2021-03-23 23:43:01 -0400

    refractored items

--- Tasktower.BoardService/Startup.cs
+++ Tasktower.BoardService/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -14,10 +15,11 @@ using Microsoft.Extensions.Logging;
 using Tasktower.BoardService.Errors.Middleware.Extensions;
 using Tasktower.BoardService.Security.Extensions;
 using Tasktower.BoardService.Errors.Middleware;
-using Tasktower.BoardService.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Converters;
 using Tasktower.BoardService.DataAccess.Context;
+using Tasktower.BoardService.Security;
 using Tasktower.BoardService.Tools.DependencyInjection.Extensions;
 
 namespace Tasktower.BoardService
@@ -34,10 +36,26 @@ namespace Tasktower.BoardService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AuthStartupOptionsRunner.ConfigureAuthentication)
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = HeaderAuthenticationOptions.DefaultScheme;
+                    options.DefaultChallengeScheme = HeaderAuthenticationOptions.DefaultScheme;
+                })
                 .AddHeaderAuthentication(options => { });
-            services.AddAuthorization(AuthStartupOptionsRunner.ConfigureAuthorization);
+            
+            services.AddAuthorization(options =>
+            {
+                var policies = typeof(Policies).GetFields(BindingFlags.Public | BindingFlags.Static)
+                    .Where(f => f.FieldType == typeof(Policy))
+                    .Select(f => (Policy)f.GetValue(null));
+                foreach (var policy in policies)
+                {
+                    options.AddPolicy(policy);
+                }
+            });
+            
             services.AddHttpContextAccessor();
+            
             services.AddDbContext<BoardDBContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SQLServerBoardDB"));
@@ -52,7 +70,71 @@ namespace Tasktower.BoardService
                     options.SerializerSettings.Converters.Add(new StringEnumConverter());
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
-            services.AddSwaggerGen(SwaggerStartupOptionsRunner.ConfigureSwaggerGen);
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tasktower.BoardService", Version = "3.0.0" });
+            var useridScheme = new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = SecurityHeaderNames.UserId,
+                Reference = new OpenApiReference
+                {
+                    Id = "userid header",
+                    Type = ReferenceType.SecurityScheme
+                }
+
+            };
+            c.AddSecurityDefinition(useridScheme.Reference.Id, useridScheme);
+
+            var nameScheme = new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = SecurityHeaderNames.Name,
+                Reference = new OpenApiReference
+                {
+                    Id = "name header",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            c.AddSecurityDefinition(nameScheme.Reference.Id, nameScheme);
+            var emailScheme = new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = SecurityHeaderNames.Email,
+                Reference = new OpenApiReference
+                {
+                    Id = "email header",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            c.AddSecurityDefinition(emailScheme.Reference.Id, emailScheme);
+            var rolesScheme = new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = SecurityHeaderNames.Roles,
+                Reference = new OpenApiReference
+                {
+                    Id = "roles header",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            c.AddSecurityDefinition(rolesScheme.Reference.Id, rolesScheme);
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {useridScheme, Array.Empty<string>()},
+                    {nameScheme, Array.Empty<string>()},
+                    {emailScheme, Array.Empty<string>()},
+                    {rolesScheme, Array.Empty<string>()}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,7 +144,10 @@ namespace Tasktower.BoardService
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(SwaggerStartupOptionsRunner.ConfigureSwaggerUI);
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tasktower.BoardService v1");
+                });
             }
 
             app.UseCustonErrorHandler(new ErrorHandleMiddlewareOptions
